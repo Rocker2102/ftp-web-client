7c1a1e15b7d7b68b42612328e9dfe7823845bfc5
Ankush Yadav <ankushyadav9302@gmail.com>
2020-05-06 23:33:11 +0530

    added directory switching (backend)

---
+++ server/chdir.php
@@ -0,0 +1,77 @@
+<?php
+    $send = new stdClass;
+    $details = [];
+
+    require "operations.php";
+
+    $requiredKeys = ["dir"];
+    $data = verifyData($_POST, $requiredKeys);
+
+    if (!$data) {
+        exitScript($send, 1, "Bad Request!");
+    } else if (!getSessionVar("FTP_Status")) {
+        exitScript($send, 1, "Invalid Session!");
+    } else {
+        $pwd = getSessionVar("FTP_Cd");
+        if ($pwd == "/" || $pwd == ".")  {
+            $chdir = $data["dir"];
+        } else {
+            $chdir = $pwd."/".$data["dir"];
+        }
+        $ip = getSessionVar("FTP_Host");
+        $port = getSessionVar("FTP_Port");
+
+        require "ftp_operations.php";
+        $ftp = new ftp_operations($ip, $port);
+
+        if ($username = getSessionVar("FTP_Username")) {
+            if (!getSessionVar("FTP_Password")) {
+                $password = "";
+            } else {
+                $password = getSessionVar("FTP_Password");
+            }
+    
+            if ($ftp->login($username, $password)) {
+                $send->status = "Connected to ".$ip.":".$port."@".$username;
+            } else {
+                $send->status = "Unable to connect with given username & password!";
+                exitScript($send, 1, "Unable to connect to host at ".$ip.":".$port." with given credentials!");
+            }
+        } else if ($ftp->login()) {
+            $send->status = "Connected to ".$ip.":".$port;
+        } else if ($ftp->login("anonymous")) {
+            $send->status = "Connected to ".$ip.":".$port."@anonymous";
+        } else {
+            $send->status = "Unable to connect to host at ".$ip.":".$port;
+            exitScript($send, 1, "Anonymous users are not allowed!");
+        }
+
+        if (!$ftp->ftpStatus()) {
+            exitScript($send, 1, "Unable to connect to host at ".$ip.":".$port);
+        }
+
+        if ($ftp->chdir($chdir)) {
+            $dirDetailed = $ftp->getRawList();
+            $dirParsed = $ftp->getMlsd();
+            setSessionVar("FTP_Cd", $ftp->getPwd());
+        } else if ($ftp->getLastModifiedTime($chdir)) {
+            exitScript($send, 1, "Selected object cannot be opened!");
+        } else {
+            exitScript($send, 1, "Unable to change directory to ".$chdir);
+        }
+
+        $send->detailed = $dirDetailed;
+        $send->parsed = $dirParsed;
+
+        if (is_array($dirParsed) && count($dirParsed) > 1) {
+            $send->dir = formArr($dirDetailed, $dirParsed, $ftp->getPwd());
+            setSessionVar("FTP_Cd", $ftp->getPwd());
+            $send->pwd = getSessionVar("FTP_Cd");
+            exitScript($send, 0, "Connection established!");
+        } else {
+            $send->status = "Connected. No directories/files to list!";
+            $send->dir = [];
+            exitScript($send, 0, "No directories to list!");
+        }
+    }
+?>
